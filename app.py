517673fa7787f68fb0f19b92a95fe04e46075491
Garrit Wilson <wilsongarrit@gmail.com>
2022-10-06 10:43:38 +0200

    setup basic streamlit app and folder structure

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.markdown("# Customer Lifetime Value Prediction")
+
+st.text('')
+st.text('')
+st.text('')
+
+st.text("Please upload transaction data here:")
+
+st.text('')
+st.text('')
+st.text('')
+
+st.file_uploader("Transaction Data")
