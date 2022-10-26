3f66c237e59010685ef1eb49fa6c80ba59e29aa3
Garrit Wilson <wilsongarrit@gmail.com>
2022-10-26 12:42:30 +0200

    working on new approach to dataframe slicing based on timeframes in copy of clv_prediction.ipynb

--- modules/modules.py
+++ modules/modules.py
@@ -13,26 +13,32 @@ from sklearn.model_selection import train_test_split, cross_validate
 def clean_dataframe(df):
     '''Function to clean dataframe from missing data, outliers, duplicates,
     and to change data types as necessary.'''
-         
-    ## dropping outliers and negative values in Quantity
-    idx_neg = df.loc[df.Quantity < 0].index
     
-    df.drop(idx_neg, inplace= True)
-    
-    ## removing outliers
-    num_cols = df.select_dtypes(include=["int64", "float64"]).columns
-    
-    for col in num_cols:
+    if "Revenue" in df.columns:
         
-        mask = df[col] > df[col].quantile(0.99)
+        df["InvoiceDate"] = pd.to_datetime(df["InvoiceDate"])
+    
+    else:
+            
+        ## dropping outliers and negative values in Quantity
+        idx_neg = df.loc[df.Quantity < 0].index
+        
+        df.drop(idx_neg, inplace= True)
+        
+        ## removing outliers
+        num_cols = df.select_dtypes(include=["int64", "float64"]).columns
+        
+        for col in num_cols:
+            
+            mask = df[col] > df[col].quantile(0.99)
 
-        df.drop(df[mask].index, inplace=True)
+            df.drop(df[mask].index, inplace=True)
+            
+        ## changing datatype for InvoiceDate
+        df["InvoiceDate"] = pd.to_datetime(df.InvoiceDate)
         
-    ## changing datatype for InvoiceDate
-    df["InvoiceDate"] = pd.to_datetime(df.InvoiceDate)
-    
-    ## creating revenue column
-    df["Revenue"] = df["Quantity"] * df["UnitPrice"]
+        ## creating revenue column
+        df["Revenue"] = df["Quantity"] * df["UnitPrice"]
 
     return df
 
